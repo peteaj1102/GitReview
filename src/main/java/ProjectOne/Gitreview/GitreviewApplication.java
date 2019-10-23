05a4cdad0687f515e2fe76d5960b40d1e3087ee5
Petru Jelehovschi <11.fenix.96@gmail.com>
2019-10-23 21:51:59 +0300

    fisrt commit

---
+++ src/main/java/ProjectOne/Gitreview/GitreviewApplication.java
@@ -0,0 +1,13 @@
+package ProjectOne.Gitreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitreviewApplication.class, args);
+	}
+
+}
